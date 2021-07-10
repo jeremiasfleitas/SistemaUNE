bd5f360036ece7b174f1afe46f40e311f9fec8b8
DESKTOP-G7V4TI3\Fernando <nandoga2008@gmail.com>
2021-07-10 15:17:36 -0400

    no message

--- src/sistemaune/SistemaUNE.java
+++ src/sistemaune/SistemaUNE.java
@@ -15,7 +15,8 @@ public class SistemaUNE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ABM_Main main = new ABM_Main();
+        main.setVisible(true);
     }
     
 }
